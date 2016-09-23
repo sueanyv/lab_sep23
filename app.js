437c6d57a0cde73eecead079c83dcd4dcfc3cf1d
sueanyv <valenciasue@hotmail.com>
2016-09-23 16:16:19 -0700

    finished question3, ran test and it works

--- app.js
+++ app.js
@@ -55,10 +55,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  var totalSum = sum(a, (sum(b, c))).toString();
-  var multiplyNumbers = multiply(a, multiply(b, c)).toString();;
-  var addMessage = a + ' and ' + b + ' and ' + c + 'add up to' + totalSum + '.';
-  var sumandMultiplymessage = ' The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyNumbers;
+  var totalSum = sum(a, sum(b, c)[0])[0];
+  var multiplyNumbers = multiply(a, multiply(b, c)[0])[0];
+  var addMessage = a + ' and ' + b + ' and ' + c + ' sum to ' + totalSum + '.';
+  var sumandMultiplymessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyNumbers + '.';
   var lastLine = [totalSum, multiplyNumbers, addMessage, sumandMultiplymessage];
   return lastLine;
 }
