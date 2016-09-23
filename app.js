ff935f7b5e874929f03ecb1d70c4cd11cae1eb4f
sueanyv <valenciasue@hotmail.com>
2016-09-23 12:08:37 -0700

    updates

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum (a, b){
+function sum(a, b) { //eslint-disable-line
   var sum = a + b;
   var message = 'the sum of' + a + ' and ' + b + 'is ' + sum + ',';
   var output = [sum, message];
@@ -16,7 +16,7 @@ function sum (a, b){
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testsum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
