01d50805502cab02dcd4173f6a55640c05adf206
sueanyv <valenciasue@hotmail.com>
2016-09-23 17:01:36 -0700

    started question 4

--- app.js
+++ app.js
@@ -81,7 +81,10 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(testArray) { //eslint-disable-line
-
+  var arraySum = sum(a, sum(b, c)[0])[0];
+  var arraymessage = a + +b + c + ' was passed in as an array of numbers, and ' + arraySum + '.';
+  var lastLine = [arraySum, arraymessage];
+  return lastLine;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
